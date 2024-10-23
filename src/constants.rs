d12a6cf953ec346da5fede45f331c422d7694f43
Kuly14 <kulman.david@gmail.com>
2024-10-23 21:14:07 +0200

    Add empty sha3 hash as a constant

---
+++ src/constants.rs
@@ -0,0 +1,6 @@
+use alloy_primitives::{b256, B256};
+
+/// Sha3_256 over empty array.
+pub const SHA_EMPTY: B256 =
+    b256!("a7ffc6f8bf1ed76651c14756a061d662f580ff4de43b49fa82d80a4b80f8434a");
+
--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,4 @@
 mod sha3;
 pub use sha3::{Sha3, sha3, eip191_hash_message, eip191_message};
+
+pub mod constants;
