2fd9488a413e01b4db6e3230240f61b3b76a2d19
Kuly14 <kulman.david@gmail.com>
2024-12-09 16:23:08 +0100

    import the Signature type from base-rs

--- src/lib.rs
+++ src/lib.rs
@@ -2,8 +2,8 @@ mod sha3;
 pub use sha3::{Sha3, sha3, eip191_hash_message, eip191_message};
 
 pub mod constants;
-mod signature;
 
 use alloy_primitives::FixedBytes;
 pub type B1368 = FixedBytes<1368>;
 
+pub use base_primitives::{Signature, SignatureError};
